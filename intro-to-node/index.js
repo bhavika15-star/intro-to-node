be1badc8aa610390feca93fcbf763b65251edcdf
Bhavika Chauhan <63841109+bhavika15-star@users.noreply.github.com>
2021-06-11 17:21:17 +0530

    It is made using node.js
    I will create the random names of superheroes and supervillains.

---
+++ intro-to-node/index.js
@@ -0,0 +1,9 @@
+//jshint esversion:6
+var supervillains = require("supervillains");
+var superheros = require("superheroes");
+
+var mySupervillainsName=supervillains.random();
+var mySuperherosName=superheros.random();
+
+console.log(mySupervillainsName);
+console.log(mySuperherosName);
